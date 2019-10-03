456802126c41524813a8ab94b368b8fd99b89100
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-10-03 01:34:11 -0400

    move POST to "/:id/posts" back to postRouter

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const userRouter = require("./users/userRouter")
-const postRouter = require("./posts/postRouter")
 const Posts = require("./posts/postDb")
 const Users = require("./users/userDb")
 
@@ -13,20 +12,6 @@ const {
 const server = express()
 
 server.use("/", express.json(), logger, userRouter)
-server.use("/:id/posts", postRouter)
-
-server.post("/:id/posts", validatePost, async (req, res) => {
-  console.log("ID:", req.params.id)
-  try {
-    var newPost = await Posts.insert({
-      user_id: req.params.id,
-      text: req.body.text
-    })
-    res.status(200).json(newPost)
-  } catch (e) {
-    res.status(500).json({ error: "Error while adding post to database" })
-  }
-})
 
 //custom middleware
 
